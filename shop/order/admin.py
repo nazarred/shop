cba02e833173b48cf3305c3d7bc444738266a5e7
Nazar <rednaz1990@gmail.com>
2018-03-27 13:59:01 +0300

    обмежив виборку продуктів для створенних заказів

--- shop/order/admin.py
+++ shop/order/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Order
 
-admin.site.register(Order)
 
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'add_date', 'get_total_price']
+    list_filter = ['add_date']
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(OrderAdmin, self).get_form(request, obj, **kwargs)
+        if obj:
+            form.base_fields['products'].queryset = obj.products.all()
+        return form
+
+
+admin.site.register(Order, OrderAdmin)
