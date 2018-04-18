2058cec0411003930afc755724ce1368f96d403d
Nazar Redko <rednaz1990@gmail.com>
2018-04-18 12:29:57 +0300

    delete folder venv

--- shop/shop/local_settings.py
+++ shop/shop/local_settings.py
@@ -14,10 +14,3 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 # }
 
 
-
-
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = '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'
-
